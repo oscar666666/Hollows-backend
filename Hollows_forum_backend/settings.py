941b0dc9371853c5038a77d0599ba7b7f296cc8a
oscar666666 <oscar88feichang6@gmail.com>
2020-02-15 18:21:30 -0600

    nn

--- Hollows_forum_backend/settings.py
+++ Hollows_forum_backend/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hollowsforum',
         'USER': 'hollowsuser',
-        'PASSWORD': '6yvENjimEq',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
